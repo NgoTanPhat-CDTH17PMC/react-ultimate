568f9fe3462cd05bd2281900331f57399d20129d
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-02 17:09:57 +0700

    state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,10 +4,20 @@
 import React from 'react'; // import thư viện react
 
 class MyComponent extends React.Component { // ke thua tinh nag cua react component
+
+    state = {
+        name: 'eric',
+        address: 'hoi dan it',
+        age: 27
+    };
+
     render() { //tap hop nhung nguyen lieu, duc ket duoc 1 cai gi day
         return (
-            <div>my first component <br></br>
-            {Math.random()}
+            <div>
+                my first component <br></br>
+                {Math.random()}
+                <br></br>
+                My name is {this.state.name} and i'm from {this.state.address}, I'm {this.state.age} year old!
             </div>
         );
     }
