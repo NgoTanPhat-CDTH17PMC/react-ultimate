7dd85cc656e3dd90f92938bdb203b6f750db6155
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-03 15:10:37 +0700

    Conditional Output

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,23 +1,45 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+
+  handleShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
+
   render() {
     //props => Properties giao tiep giua cac component
 
     const { listUsers } = this.props;
+
     return (
       <div>
-        {/* <p>My name is {name}</p>
-        <p>My age is {age}</p> */}
-        {listUsers.map((user, index) => {
-          return (
-            <div key={user.id}>
-              <p>My name is {user.name}</p>
-              <p>My age is {user.age}</p>
-              <hr />
-            </div>
-          );
-        })}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowListUser ? "Hide list users:" : "Show list users"}
+          </span>
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user, index) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <p>My name is {user.name}</p>
+                  <p>My age is {user.age}</p>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
