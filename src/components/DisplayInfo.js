02acc98f7c4bc08f59fb18f63cac98b5f4a090af
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-03 17:09:13 +0700

    Image

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfo.scss";
+import logo from "./../logo.svg";
 
 class DisplayInfo extends React.Component {
   state = {
@@ -19,6 +20,7 @@ class DisplayInfo extends React.Component {
 
     return (
       <div className="display-info-container">
+        <img src={logo} alt="logo" />
         <div>
           <span
             onClick={() => {
