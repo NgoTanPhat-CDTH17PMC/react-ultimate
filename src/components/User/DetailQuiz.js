7c55943afc5d3a751f813f6b0491337521cedf69
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-14 16:09:39 +0700

    Xu ly data phia Frontend

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../sevices/apiServices";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
   const param = useParams();
@@ -12,6 +13,33 @@ const DetailQuiz = (props) => {
 
   const fetchQuestion = async () => {
     let res = await getDataQuiz(quizId);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        .groupBy("id")
+        .map((value, key) => {
+          let answer = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answer.push(item.answer);
+          });
+
+          return {
+            questionId: key,
+            answer,
+            questionDescription,
+            image,
+          };
+        })
+        .value(); // gop theo ID
+      //https://prnt.sc/skqoy6QDG9rk
+      console.log(data);
+    }
   };
   return <div className="detail-quiz-container">Detail Quiz</div>;
 };
